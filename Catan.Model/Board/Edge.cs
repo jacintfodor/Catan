9dac4ec4b05d8e9b3960507a0f37c7ab1f5feb6c
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-18 06:42:57 +0100

    created Catan.Model Package and moved Catan to Catan.model

---
+++ Catan.Model/Board/Edge.cs
@@ -0,0 +1,14 @@
+﻿namespace Catan.Model
+{
+    public class Edge
+    {
+        public Edge(Player owner)
+        {
+            Owner = owner;
+        }
+
+
+        //is there is owner there is road
+        public Player Owner { get; set; }
+    }
+}
---
+++ Catan.Model/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Catan.Model
+{
+    public class Player
+    {
+        public int[] resources;//enum can be converted to int and vica versa, indexes will equel converted resource int
+        private int numberOfSettlemensOwned;
+        private int numberOfTownsOwned;
+        private int numberOfScoreCardsOwned;
+        private int numberOfHorseCardsOwned;
+        private int numberOfRoadCarsOwned;
+        private int numberOfSettlementCardsOwned;
+        private int numberOfTownCradsOwned;
+
+        private String name;
+
+        public Player(String name) {
+            this.name = name;
+            //fancy way to get max value of enum.
+            //(int)Enum.GetValues(typeof(ResourceEnum)).Cast<ResourceEnum>().Max() 
+            resources = new int[5];
+        }
+
+        public String getName() { return name; }
+        public int getScore() {
+            
+            int retVal = 0;
+            retVal += numberOfSettlemensOwned;
+            retVal += numberOfTownsOwned;
+            //no ref yet
+            //retVal += name == LargestArmyHolder.owner ? LargestArmyHolder.score() : 0;
+            //retVal += name == LongestRoadOwner.owner ? LongestRoadOwner.score() : 0;
+            
+            return retVal;
+
+        }
+    
+    
+    }
+}
