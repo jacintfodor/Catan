2f454b905051eab3fdd7f30e542b8f5dc3e16d96
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-19 11:11:04 +0100

    added NotPlayer

---
+++ Catan.Model/Context/Players/NotPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Context.Players
+{
+    public class NotPlayer : IPlayer
+    {
+        private NotPlayer()
+        {
+        }
+
+        private static readonly NotPlayer _instance = new NotPlayer();
+        public static NotPlayer Instance
+        { get { return _instance; } }
+
+        public int CalculateScore()
+        {
+            return -1;
+        }
+
+        public int LengthOfLongestRoad()
+        {
+            return 4;
+        }
+
+        public int NumberOfKnightsOwned()
+        {
+            return 2;
+        }
+    }
+}
