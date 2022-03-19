e27e68c838a36e7f17f5d44ba033b49a65177f5d
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-19 10:54:58 +0100

    ITitle LongestRoadOwner implemented

---
+++ Catan.Model/Context/Titles/LongestRoadOwner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Context.Titles
+{
+    internal class LongestRoadOwner : ITitle
+    {
+        public void processOwner(IPlayer titleContester)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int score()
+        {
+            return 2;
+        }
+    }
+}
