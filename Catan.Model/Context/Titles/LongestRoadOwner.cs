9cf7f68536a49b2a8ca06a872b1b3aa8cf3760e1
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-19 11:21:54 +0100

    singleton

--- Catan.Model/Context/Titles/LongestRoadOwner.cs
+++ Catan.Model/Context/Titles/LongestRoadOwner.cs
@@ -13,6 +13,10 @@ namespace Catan.Model.Context.Titles
 
         private LongestRoadOwner() { Owner = NotPlayer.Instance; }
 
+        private static readonly LongestRoadOwner _instance = new();
+        public static LongestRoadOwner Instance
+        { get { return _instance; } }
+
         public void processOwner(IPlayer titleContester)
         {
             if (titleContester.LengthOfLongestRoad() > Owner.LengthOfLongestRoad())
