3a638573694ef5a2bf037f2e2a77332fda884bbf
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-20 19:59:01 +0100

    added Goods

--- Catan.Model/Board/Edge.cs
+++ Catan.Model/Board/Edge.cs
@@ -12,5 +12,8 @@ namespace Catan.Model
 
         //is there is owner there is road
         public IPlayer Owner { get; set; }
+
+        //Crop, Ore, Wood, Brick, Wool
+        public Goods Cost { get => new Goods( new List<int>() {0,0,1,1,0 }); }
     }
 }
---
+++ Catan.Model/Context/Goods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Context
+{
+    public class Goods
+    {
+        private Dictionary<ResourceEnum, int>? _goods;
+
+        public int Crop { get => _goods[ResourceEnum.Crop]; }
+        public int Ore { get => _goods[ResourceEnum.Ore]; }
+        public int Wood { get => _goods[ResourceEnum.Wood]; }
+        public int Brick { get => _goods[ResourceEnum.Brick]; }
+        public int Wool { get => _goods[ResourceEnum.Wool]; }
+
+
+        public Goods(List<int> l)
+        {
+            if (l.Count != 5) throw new InvalidDataException("mismatching list count");
+            if (!l.TrueForAll(n => n >= 0)) throw new InvalidDataException("negative");
+
+
+            foreach (ResourceEnum resource in Enum.GetValues(typeof(ResourceEnum)))
+            {
+                if((int)resource > (int)ResourceEnum.Desert)
+                {
+                    _goods[resource] = l[(int)resource];
+                }
+            }
+        }
+
+        public Goods() : this(Enumerable.Repeat(0, 5).ToList())
+        {
+        }
+
+        //Crop, Ore, Wood, Brick, Wool
+        public static Goods operator +(Goods a, Goods b)
+        => new Goods(new List<int>() {
+                a.Crop + b.Crop,
+                a.Ore + b.Ore,
+                a.Wood + b.Wood,
+                a.Brick + b.Brick,
+                a.Wool + b.Wool
+        });
+
+        public static Goods operator -(Goods a, Goods b)
+        => new Goods(new List<int>() {
+                a.Crop - b.Crop,
+                a.Ore - b.Ore,
+                a.Wood - b.Wood,
+                a.Brick - b.Brick,
+                a.Wool - b.Wool
+        });
+
+    }
+}
