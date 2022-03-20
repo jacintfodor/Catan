8caa21e1795adb3fbf27e60246dfdc6a193a7190
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-20 18:44:19 +0100

    CatanGameState interface created

---
+++ Catan.Model/ICatanGameState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model
+{
+    interface ICatanGameState
+    {
+        public void EndTurn(CatanContext context);
+        public void ThrowDices(CatanContext context);
+        public void MoveRogue(CatanContext context);
+        public void ExchangeWithBank(CatanContext context);
+        public void PurchaseBonusCard(CatanContext context);
+        public void BuildRoad(CatanContext context);
+        public void BuildSettleMent(CatanContext context);
+        public void UpgradeSettleMentToTown(CatanContext context);
+        public void StartTrade(CatanContext context);
+        public void AcceptTrade(CatanContext context);
+        public void DenyTrade(CatanContext context);
+        public bool IsGameInProgress(CatanContext context);
+        public bool IsTradeInProgress(CatanContext context);
+        public bool HasEnoughResources(CatanContext context);
+
+    }
+}
