20935d23cb16b45cf6d6f18e6f025b3b9b4673b1
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-21 21:12:58 +0100

    add event

---
+++ Catan.Model/Events/DicesThrownEventArg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Events
+{
+    public  class DicesThrownEventArg : EventArgs
+    {
+        public int FirstDice { get; private set; }
+        public int SecondDice { get; private set;}
+
+        public DicesThrownEventArg(int a, int b)
+        {
+            FirstDice = a;
+            SecondDice = b;
+        }
+    }
+}
