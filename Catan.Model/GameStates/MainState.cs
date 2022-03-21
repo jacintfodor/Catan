92443a430def5214c317683087e02c846c45b301
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-21 13:51:40 +0100

    MainState Partially implemented

---
+++ Catan.Model/GameStates/MainState.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catan.Model.Context.Players;
+using Catan.Model.Board.Buildings;
+
+namespace Catan.Model.GameStates
+{
+    internal class MainState : ICatanGameState
+    {
+        public void AcceptTrade(CatanContext context)
+        {
+            
+        }
+
+        public void DenyTrade(CatanContext context)
+        {
+            
+        }
+
+        public void EndTurn(CatanContext context)
+        {
+            if (context.Winner != NotPlayer.Instance)
+                context.EndTurn();
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void ExchangeWithBank(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasEnoughResourcesToBuildRoad(CatanContext context)
+        {
+            return context.CurrentPlayer.CanAfford(Road.BuildCost);
+        }
+
+        public bool HasEnoughResourcesToBuildSettlement(CatanContext context)
+        {
+            return context.CurrentPlayer.CanAfford(Settlement.BuildCost);
+        }
+
+        public bool HasEnoughResourcesToUpgradeSettlementToTown(CatanContext context)
+        {
+            return context.CurrentPlayer.CanAfford(Town.BuildCost);
+        }
+
+        public bool IsGameInProgress(CatanContext context)
+        {
+            return context.Winner == NotPlayer.Instance;
+        }
+
+        public bool IsTradeInProgress(CatanContext context)
+        {
+            return false;
+        }
+
+        public void MoveRogue(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseBonusCard(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThrowDices(CatanContext context)
+        {
+            context.FirstDice.roll();
+            context.LastDice.roll();
+
+            //here goes event
+        }
+    }
+}
--- Catan.Model/ICatanGameState.cs
+++ Catan.Model/ICatanGameState.cs
@@ -13,15 +13,33 @@ namespace Catan.Model
         public void MoveRogue(CatanContext context);
         public void ExchangeWithBank(CatanContext context);
         public void PurchaseBonusCard(CatanContext context);
-        public void BuildRoad(CatanContext context);
-        public void BuildSettleMent(CatanContext context);
-        public void UpgradeSettleMentToTown(CatanContext context);
+        public void BuildRoad(CatanContext context, int row, int col)
+        {
+            if (HasEnoughResourcesToBuildRoad(context))
+                context.Board.buildRoad(row, col, context.CurrentPlayer);
+        }
+        public void BuildSettleMent(CatanContext context, int row, int col)
+        {
+            if (HasEnoughResourcesToBuildSettlement(context))
+                context.Board.buildSettlement(row, col, context.CurrentPlayer);
+        }
+        public void UpgradeSettleMentToTown(CatanContext context, int row, int col)
+        {
+            if (HasEnoughResourcesToUpgradeSettlementToTown(context) && IsSettlementOwnedByCurrentPlayer(context))
+                context.Board.buildSettlement(row, col, context.CurrentPlayer);
+        }
         public void StartTrade(CatanContext context);
         public void AcceptTrade(CatanContext context);
         public void DenyTrade(CatanContext context);
         public bool IsGameInProgress(CatanContext context);
         public bool IsTradeInProgress(CatanContext context);
-        public bool HasEnoughResources(CatanContext context);
+        public bool HasEnoughResourcesToBuildRoad(CatanContext context);
+        public bool HasEnoughResourcesToBuildSettlement(CatanContext context);
+        public bool HasEnoughResourcesToUpgradeSettlementToTown(CatanContext context);
+        public bool IsSettlementOwnedByCurrentPlayer(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
