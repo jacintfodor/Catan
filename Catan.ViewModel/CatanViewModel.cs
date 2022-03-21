9d8c2e494a34c6b337a3fcc4e61030be3810f8c8
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-21 21:16:12 +0100

    Connected model and View in terms of throwing dices

--- Catan.ViewModel/CatanViewModel.cs
+++ Catan.ViewModel/CatanViewModel.cs
@@ -11,14 +11,31 @@ namespace Catan.ViewModel
     public class CatanViewModel : ViewModelBase
     {
         private CatanGameModel _model;
+        
+        int _firstDiceValue = 1;
+        int _secondDiceValue = 1;
+        public int FirstDiceFace { get => _firstDiceValue; set { _firstDiceValue = value; OnPropertyChanged(); OnPropertyChanged(nameof(SumOfDices)); } }
+        public int SecondDiceFace { get => _secondDiceValue; set { _secondDiceValue = value; OnPropertyChanged(); OnPropertyChanged(nameof(SumOfDices)); } }
+        public int SumOfDices { get => FirstDiceFace + SecondDiceFace; }
+
+
+        public DelegateCommand ThrowDicesCommand { get; private set; }
+
+        
 
         public CatanViewModel(CatanGameModel model)
         {
             _model = model;
+            _model.DicesThrown += Model_DicesThrown;
 
+            ThrowDicesCommand = new DelegateCommand(_ => _model.ThrowDices());
 
         }
 
-
+        private void Model_DicesThrown(object? sender, Model.Events.DicesThrownEventArg e)
+        {
+            FirstDiceFace = e.FirstDice;
+            SecondDiceFace = e.SecondDice;
+        }
     }
 }
