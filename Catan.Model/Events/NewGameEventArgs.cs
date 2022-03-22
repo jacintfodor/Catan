008acb998b8239ddb2bf67ad5b5e4f0a6603ac44
kov-Tamas <kovvacs99@gmail.com>
2022-03-22 19:38:36 +0100

    new game event args created

---
+++ Catan.Model/Events/NewGameEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using Catan.Model.Board.Compontents;
+
+namespace Catan.Model.Events
+{
+    public class NewGameEventArgs : EventArgs
+    {
+        public NewGameEventArgs(Hex[,] hexes, Vertex[,] vertices, Edge[,] edges)
+        {
+            Hexes = hexes;
+            Vertices = vertices;
+            Edges = edges;
+        }
+
+        public Hex[,] Hexes { get; set; }
+        public Vertex[,] Vertices { get; set; }
+        public Edge[,] Edges { get; set; }
+
+
+
+
+    }
+}
