e39503ca767f6927abc3e352b95ce7572fd68869
y86qk2 <y86qk2@INF.ELTE.HU>
2022-03-23 01:33:20 +0100

    extended event with player resource its a temporary solution, should have its own event

--- Catan.Model/Events/DicesThrownEventArg.cs
+++ Catan.Model/Events/DicesThrownEventArg.cs
@@ -4,17 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Catan.Model.Context;
+
 namespace Catan.Model.Events
 {
-    public  class DicesThrownEventArg : EventArgs
+    public class DicesThrownEventArg : EventArgs
     {
         public int FirstDice { get; private set; }
-        public int SecondDice { get; private set;}
+        public int SecondDice { get; private set; }
 
-        public DicesThrownEventArg(int a, int b)
+
+        //Crop, Ore, Wood, Brick, Wool
+        public int CropCount { get; private set; }
+        public int OreCount { get; private set; }
+        public int WoodCount { get; private set; }
+        public int BrickCount { get; private set; }
+        public int WoolCount { get; private set; }
+
+        public DicesThrownEventArg(int a, int b, Goods g)
         {
             FirstDice = a;
             SecondDice = b;
+
+            CropCount = g.Crop;
+            OreCount = g.Ore;
+            WoodCount = g.Wood;
+            BrickCount = g.Brick;
+            WoolCount = g.Wool;
         }
     }
 }
