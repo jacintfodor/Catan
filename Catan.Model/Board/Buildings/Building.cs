4bd32c3d9c0c245fc3fdc6c0773655decbd5c955
kov-Tamas <kovvacs99@gmail.com>
2022-04-05 19:34:45 +0200

    build abstract class updated accordingly

--- Catan.Model/Board/Buildings/Building.cs
+++ Catan.Model/Board/Buildings/Building.cs
@@ -1,8 +1,12 @@
-﻿namespace Catan.Model.Board.Buildings
+﻿using Catan.Model.Context.Players;
+
+namespace Catan.Model.Board.Buildings
 {
     public abstract class Building
     {
         public abstract int score();
         public abstract int amount();
+        public abstract bool isBuildable();
+        public abstract List<Player> canBuild();
     }
 }
