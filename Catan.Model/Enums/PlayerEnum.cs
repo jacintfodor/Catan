51e9941f38475e89d51cbb8447e4ba73bea5815e
Attila <azrc41@inf.elte.hu>
2022-04-12 14:39:09 +0200

    Create enum PlayerEnum

---
+++ Catan.Model/Enums/PlayerEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Enums
+{
+    public enum PlayerEnum
+    {
+        Player1, Player2, Player3
+    }
+}
