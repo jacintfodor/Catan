ec4effbbc6379d2c1c07566106085cfe69dacf13
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-12 11:41:30 +0200

    add method StartSettlementBuilding

--- Catan.Model/ICatanGameState.cs
+++ Catan.Model/ICatanGameState.cs
@@ -18,6 +18,7 @@ namespace Catan.Model
         public void PurchaseBonusCard(CatanContext context);
         public void StartRoadBuilding(CatanContext context);
         public void BuildRoad(CatanContext context, int row, int col);
+        public void StartSettlementBuilding(CatanContext context);
         public void BuildSettleMent(CatanContext context, int row, int col);
         public void UpgradeSettleMentToTown(CatanContext context, int row, int col);
         public void StartTrade(CatanContext context);
