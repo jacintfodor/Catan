2cbc9b099c9c7bada2aaeba47109efb18fd00e9c
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-15 16:54:03 +0200

    add EarlyRollingState

---
+++ Catan.Model/GameStates/EarlyRollingState.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catan.Model.Context;
+using Catan.Model.GameStates;
+
+namespace Catan.Model.GameStates
+{
+    public class EarlyRollingState : ICatanGameState
+    {
+        private int _rollCount = 0;
+
+        public bool IsEarlyRollingState => true;
+
+        public void AcceptTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildRoad(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildSettleMent(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cancel(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DenyTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTurn(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeWithBank(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsAffordable(CatanContext context, Goods g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRogue(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseBonusCard(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollDices(CatanContext context)
+        {
+            ++_rollCount;
+            context.FirstDice.roll();
+            context.SecondDice.roll();
+
+            context.Events.OnDiceThrown(context);
+            if (_rollCount == 3)
+                context.SetContext(new MainState());
+        }
+
+        public void StartRoadBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementUpgrading(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeSettleMentToTown(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
