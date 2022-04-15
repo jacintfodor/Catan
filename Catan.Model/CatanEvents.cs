3c0a7f6926b5c89d6db4b6157f8b587752bf62db
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-15 15:50:30 +0200

    move Events

--- Catan.Model/CatanEvents.cs
+++ Catan.Model/CatanEvents.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Catan.Model.Events;
+
 namespace Catan.Model
 {
     public class CatanEvents
@@ -16,6 +18,12 @@ namespace Catan.Model
         public static CatanEvents Instance
         { get { return _instance; } }
 
+        public event EventHandler<DicesThrownEventArg> DicesThrown;
+        public event EventHandler<GameStartEventArgs> GameStart;
 
+        public void OnGameStart(CatanContext ctx)
+        {
+            GameStart?.Invoke(this, new GameStartEventArgs(ctx.Board.Hexes, ctx.Board.Vertices, ctx.Board.Edges));
+        }
     }
 }
