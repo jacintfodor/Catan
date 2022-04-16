6d0317708c42abcbd2d4529a8f1007faef06fe3b
kov-Tamas <kovvacs99@gmail.com>
2022-04-16 14:16:04 +0200

    IVertex implemented

---
+++ Catan.Model/Board/Components/IVertex.cs
@@ -0,0 +1,22 @@
+﻿using Catan.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Board.Components
+{
+    interface IVertex
+    {
+        public PlayerEnum Owner { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool IsNotBuildable { get; set; }
+
+        public void AddPotentialBuilder(PlayerEnum player);
+        public bool IsBuildableByPlayer(PlayerEnum player);
+        public void Build(PlayerEnum player);
+        public void Upgrade();
+    }
+}
