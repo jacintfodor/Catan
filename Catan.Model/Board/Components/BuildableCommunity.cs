7c4dfd15083f6950967987a28d101a33c8f48126
kov-Tamas <kovvacs99@gmail.com>
2022-04-16 15:18:40 +0200

    BuildableCommunity implemented

---
+++ Catan.Model/Board/Components/BuildableCommunity.cs
@@ -0,0 +1,38 @@
+﻿using Catan.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Board.Components
+{
+    public class BuildableCommunity : ICommunity
+    {
+        HashSet<PlayerEnum> _potentialBuilders;
+
+        public BuildableCommunity()
+        {
+            _potentialBuilders = new HashSet<PlayerEnum>();
+            Owner = PlayerEnum.NotPlayer;
+            IsUpgradable = false;
+            IsBuildableCommunity = true;
+        }
+
+        public PlayerEnum Owner { get; set; }
+
+        public bool IsUpgradable { get; set; }
+
+        public bool IsBuildableCommunity { get; set; }
+
+        public void AddPotentionalBuilder(PlayerEnum player)
+        {
+            _potentialBuilders.Add(player);
+        }
+
+        public bool IsBuildableByPlayer(PlayerEnum player)
+        {
+            return _potentialBuilders.Contains(player);
+        }
+    }
+}
