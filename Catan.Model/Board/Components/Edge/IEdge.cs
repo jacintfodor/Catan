45ae86d3949403432d8e3b1f07990681730e34fb
kov-Tamas <kovvacs99@gmail.com>
2022-04-16 16:08:28 +0200

    IEdge implemented

---
+++ Catan.Model/Board/Components/Edge/IEdge.cs
@@ -0,0 +1,20 @@
+﻿using Catan.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Board.Components.Edge
+{
+    interface IEdge
+    {
+        public PlayerEnum Owner { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public void AddPotentialBuilder(PlayerEnum player);
+        public bool IsBuildableByPlayer(PlayerEnum player);
+        public void Build(PlayerEnum player);
+    }
+}
