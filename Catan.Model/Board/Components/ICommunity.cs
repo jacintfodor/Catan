dfdc3896e1c29d2c1e8351420768d20770d68e49
kov-Tamas <kovvacs99@gmail.com>
2022-04-16 14:39:04 +0200

    ICommunity implemented

---
+++ Catan.Model/Board/Components/ICommunity.cs
@@ -0,0 +1,22 @@
+﻿using Catan.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Board.Components
+{
+    interface ICommunity
+    {
+        public PlayerEnum Owner { get; set; }
+
+        public bool IsUpgradable { get; set; }
+
+        public bool IsBuildableCommunity { get; set; }
+
+        public void AddPotentioalBuilder(PlayerEnum player);
+
+        public bool IsBuildableByPlayer(PlayerEnum player);
+    }
+}
--- Catan.Model/Board/Components/Vertex.cs
+++ Catan.Model/Board/Components/Vertex.cs
@@ -1,21 +1,39 @@
 ﻿using Catan.Model.Board.Buildings;
 using Catan.Model.Context;
+using Catan.Model.Enums;
 
 namespace Catan.Model.Board.Components
 {
-    public class Vertex
+    public class Vertex : IVertex
     {
-        public Vertex(IPlayer owner, Building building, bool validForBuilding = true)
+        public Vertex(PlayerEnum owner, int row, int col, bool isNotBuildable)
         {
             Owner = owner;
-            Building = building;
-            ValidForBuilding = validForBuilding;
-            EligablePlayers = new List<IPlayer> { };
+            Row = row;
+            Col = col;
+            IsNotBuildable = isNotBuildable;
         }
-        public IPlayer Owner { get; set; }
-        public Building Building { get; set; }
-        public bool ValidForBuilding { get; set; }
-        public List<IPlayer> EligablePlayers { get; set; }
+
+        public PlayerEnum Owner { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public bool IsNotBuildable { get; set; }
+
+        public void AddPotentialBuilder(PlayerEnum player)
+        {
+            
+        }
+
+        public bool IsBuildableByPlayer(PlayerEnum player)
+        {
+            return false;
+        }
+        public void Build(PlayerEnum player)
+        {
+            IsNotBuildable = true;
+        }
+        public void Upgrade()
+        {}
 
     }
 }
