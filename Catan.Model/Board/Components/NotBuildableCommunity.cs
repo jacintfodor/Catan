4f3eef79b58dd40eb2461e9d5ae95abae0f1d0f8
kov-Tamas <kovvacs99@gmail.com>
2022-04-16 15:23:05 +0200

    NotBuildableCommunity implemented

---
+++ Catan.Model/Board/Components/NotBuildableCommunity.cs
@@ -0,0 +1,30 @@
+﻿using Catan.Model.Enums;
+
+namespace Catan.Model.Board.Components
+{
+    public class NotBuildableCommunity : ICommunity
+    {
+
+        private static readonly NotBuildableCommunity _instance = new NotBuildableCommunity();
+
+        public static NotBuildableCommunity Instance
+        { get { return _instance; } }
+
+        private NotBuildableCommunity()
+        {
+            Owner = PlayerEnum.NotPlayer;
+            IsUpgradable = false;
+            IsBuildableCommunity = false;
+        }
+
+        public PlayerEnum Owner { get; set; }
+
+        public bool IsUpgradable { get; set; }
+
+        public bool IsBuildableCommunity { get; set; }
+
+        public void AddPotentionalBuilder(PlayerEnum player) { }
+
+        public bool IsBuildableByPlayer(PlayerEnum player) { return false; }
+    }
+}
