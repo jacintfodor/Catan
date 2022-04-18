42ca097a508a98f070a77743030344415af69f42
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-18 19:50:29 +0200

    add state

---
+++ Catan.Model/GameStates/EarlyRoadBuildingState.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Catan.Model;
+using Catan.Model.Context;
+
+namespace Catan.Model.GameStates
+{
+    public class EarlyRoadBuildingState : ICatanGameState
+    {
+        int _turnCount = 0;
+
+        public EarlyRoadBuildingState(int tCount)
+        {
+            _turnCount = tCount;
+        }
+
+        public void AcceptTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildRoad(CatanContext context, int row, int col)
+        {
+            //TODO reduce roadCards
+
+            context.Board.BuildRoad(row, col, context.CurrentPlayer.ID);
+            context.Events.OnRoadBuilt(context, row, col, context.CurrentPlayer.ID);
+            
+            //mark neighbouring vertexes as buildable by current player
+            context.Board.getNeighbourVerticesOfEdge(row, col).ForEach(v => v.AddPotentialBuilder(context.CurrentPlayer.ID));
+            
+            //mark neighbouring Edges as Buildable
+            //TODO use getNeighbourEdgesOfEdge later
+            context.Board.getNeighbourVerticesOfEdge(row, col).ForEach(
+                v => context.Board.getNeighborEdgesOfVertex(v.Row, v.Col).ForEach(e => e.AddPotentialBuilder(context.CurrentPlayer.ID))
+            );
+
+            //TODO remove magic number 6
+            if (_turnCount > 6 && _turnCount < 0) ; //TODO throw error
+
+            else if (_turnCount == 6)
+            {
+                context.SetContext(new MainState());
+            }
+            else
+            {
+                var list = context.Board.GetVerticesEnumerable().ToList().Where(v => !v.IsNotBuildable).ToList();
+                context.Events.OnSettlementBuildingStarted(list);
+
+                context.SetContext(new EarlySettlementBuildingState(_turnCount));
+            }
+
+            context.NextPlayer();
+        }
+
+        public void BuildSettleMent(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cancel(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DenyTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTurn(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeWithBank(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsAffordable(CatanContext context, Goods g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRogue(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseBonusCard(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollDices(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartRoadBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementUpgrading(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeSettleMentToTown(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Catan.Model/GameStates/EarlySettlementBuildingState.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Catan.Model;
+using Catan.Model.Context;
+using Catan.Model.Board.Components;
+
+namespace Catan.Model.GameStates
+{
+    public class EarlySettlementBuildingState : ICatanGameState
+    {
+        int _turnCount;
+
+        public EarlySettlementBuildingState(int tCount)
+        {
+            _turnCount = tCount;
+        }
+        public void AcceptTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildRoad(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildSettleMent(CatanContext context, int row, int col)
+        {
+            //TODO reduce players SettlementCards
+            context.Board.BuildSettlement(row, col, context.CurrentPlayer.ID);
+            context.Events.OnSettlementBuilt(context, row, col, context.CurrentPlayer.ID);
+            
+            context.Board.getNeighborEdgesOfVertex(row, col).ForEach(e => e.AddPotentialBuilder(context.CurrentPlayer.ID));
+            context.Board.getNeighborVerticesOfVertex(row, col).ForEach(v => v.SetNotBuildableCommunity());
+            
+            var list = context.Board.GetEdgesEnumerable().ToList().Where(e => e.IsBuildableByPlayer(context.CurrentPlayer.ID)).ToList();
+            context.Events.OnRoadBuildingStarted(list);
+
+            context.SetContext(new EarlyRoadBuildingState(_turnCount + 1));
+        }
+
+        public void Cancel(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DenyTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTurn(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeWithBank(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsAffordable(CatanContext context, Goods g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRogue(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseBonusCard(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollDices(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartRoadBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementUpgrading(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeSettleMentToTown(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
