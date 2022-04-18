a8ad88cc12f8c928599f9df2807e437e788281d3
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-18 19:23:56 +0200

    add eventArg

---
+++ Catan.Model/Events/RoadBuildingStartedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Catan.Model.Board.Components;
+
+namespace Catan.Model.Events
+{
+    public class RoadBuildingStartedEventArgs
+    {
+        public RoadBuildingStartedEventArgs(List<IEdge> edges)
+        {
+            Edges = edges;
+        }
+        public List<IEdge> Edges { get; private set; }
+    }
+}
