db3e2a00c77765f4e12a7e6d60fa69b0eef5e513
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-18 03:49:36 +0200

    add vertical edges

--- Catan.ViewModel/Edge/VerticalViewModel.cs
+++ Catan.ViewModel/Edge/VerticalViewModel.cs
@@ -13,5 +13,17 @@ namespace Catan.ViewModel
         public int Row { get; set; }
         public int Column { get; set; }
         public string Owner { get; set; }
+
+        public String Top
+        {
+            get => (Row * 30 - 30).ToString();
+        }
+
+        private int Offset { get => (Row % 2 == 0) ? 0 : 30; }
+
+        public String Left
+        {
+            get => (Offset + Column * 30 -30 -3).ToString();
+        }
     }
 }
