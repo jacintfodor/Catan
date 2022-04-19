56d659bcbf0fe718cea685b1b635a15c21432319
Attila <azrc41@inf.elte.hu>
2022-04-19 14:21:33 +0200

    Create Test for CubeDice

---
+++ Catan.Model.Test/CubeDiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Catan.Model.Context;
+using System.IO;
+using Catan.Model.Enums;
+namespace Catan.Model.Test
+{
+    [TestClass]
+    public class CubeDiceTest
+    {
+        [TestMethod]
+        public void Roll()
+        {
+            CubeDice dice = new CubeDice(155);
+            Assert.AreEqual(1, dice.RolledValue);
+            int sum = 0;
+            int x = 100000;
+            for (int i = 0; i < x; i++)
+            {
+                dice.roll();
+                sum += dice.RolledValue;
+                Assert.IsTrue(1 <= dice.RolledValue && dice.RolledValue <= 6);
+            }
+            float sum2 = sum;
+            float f = sum2 / x;
+            float tolerance = 0.02f;
+            // 0.01 el is mindíg lefutott de a békesség kedvéért legyen 0.02
+            float expectedValue = 3.5f;
+            Assert.IsTrue(expectedValue - tolerance < f);
+            Assert.IsTrue(f < expectedValue + tolerance);
+
+        }
+    }
+}
