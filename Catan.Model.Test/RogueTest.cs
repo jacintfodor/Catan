4801be98d7e49314f8100d137dd92ea0db72ff91
Attila <azrc41@inf.elte.hu>
2022-04-19 14:35:38 +0200

    Create Test for Rogue

---
+++ Catan.Model.Test/RogueTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Catan.Model.Context;
+
+
+namespace Catan.Model.Test
+{
+    [TestClass]
+    public class RogueTest
+    {
+        [TestMethod]
+        public void Move()
+        {
+            Rogue.Instance.Move(0, 0);
+
+            Assert.AreEqual(0, Rogue.Instance.Row);
+            Assert.AreEqual(0, Rogue.Instance.Col);
+
+            Rogue.Instance.Move(4, 1);
+
+            Assert.AreEqual(4, Rogue.Instance.Row);
+            Assert.AreEqual(1, Rogue.Instance.Col);
+
+            Rogue.Instance.Move(3, 5);
+
+            Assert.AreEqual(3, Rogue.Instance.Row);
+            Assert.AreEqual(5, Rogue.Instance.Col);
+
+        }
+
+    }
+
+}
