d4da2615af987b63c93d14d8f8f4a19c6ed175fc
kov-Tamas <kovvacs99@gmail.com>
2022-04-19 19:16:57 +0200

    bank state created

---
+++ Catan.Model/GameStates/BankState.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Catan.Model;
+using Catan.Model.Context;
+
+namespace Catan.Model.GameStates
+{
+    public class BankState : ICatanGameState
+    {
+        public bool IsBankState => true;
+
+        public BankState()
+        {
+
+        }
+
+        public void AcceptTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildRoad(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildSettleMent(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cancel(CatanContext context)
+        {
+            context.Events.OnCancel();
+            context.SetContext(new MainState());
+        }
+
+        public void DenyTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTurn(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExchangeWithBank(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsAffordable(CatanContext context, Goods g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRogue(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseBonusCard(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollDices(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartRoadBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementBuilding(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartSettlementUpgrading(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrade(CatanContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpgradeSettleMentToTown(CatanContext context, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
