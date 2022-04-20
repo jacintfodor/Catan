e855b4ea39173b17028cc5779f610a41416aeed3
y86qk2 <y86qk2@INF.ELTE.HU>
2022-04-20 02:55:02 +0200

    add eventargs

---
+++ Catan.Model/Events/RogueMovedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catan.Model.Events
+{
+    public class RogueMovedEventArgs
+    {
+        public RogueMovedEventArgs(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+    }
+}
