ddbd372fb492ba4bba56130db6f33e79283a560d
y86qk2 <y86qk2@INF.ELTE.HU>
2022-05-03 20:22:33 +0200

    extract interface

---
+++ Catan.Model/Board/ICatanBoard.cs
@@ -0,0 +1,22 @@
+﻿using Catan.Model.Board.Components;
+using Catan.Model.Enums;
+
+namespace Catan.Model.Board
+{
+    public interface ICatanBoard
+    {
+        void BuildRoad(int row, int col, PlayerEnum player);
+        void BuildSettlement(int row, int col, PlayerEnum player);
+        IEdge GetEdge(int row, int col);
+        IEnumerable<IEdge> GetEdgesEnumerable();
+        List<IEdge> GetEdgesofEdge(int row, int col);
+        IEnumerable<IHex> GetHexesEnumerable();
+        List<IEdge> GetNeighborEdgesOfVertex(int row, int col);
+        List<IVertex> GetNeighborVerticesOfVertex(int row, int col);
+        List<IVertex> GetNeighbourVerticesOfEdge(int row, int col);
+        IEnumerable<IVertex> GetVerticesEnumerable();
+        List<IVertex> GetVerticesOfHex(int row, int col);
+        void MarkNeighbouringVerticesNotBuildable(int row, int col);
+        void UpgradeSettlement(int row, int col);
+    }
+}
