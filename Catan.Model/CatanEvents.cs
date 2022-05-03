fea0ef4ec7027f491dbf579ac523aed2608f700b
y86qk2 <y86qk2@INF.ELTE.HU>
2022-05-03 19:54:19 +0200

    Revert "remove class"
    This reverts commit f32d2d13a518d96d02e2018a4a3e257c3ab143c8.

---
+++ Catan.Model/CatanEvents.cs
@@ -0,0 +1,24 @@
+﻿using Catan.Model.Board.Components;
+using Catan.Model.Context;
+using Catan.Model.Enums;
+using Catan.Model.Events;
+
+namespace Catan.Model
+{
+    public class CatanEvents
+    {
+        private CatanEvents()
+        {
+        }
+
+        public string Name { get; set; }
+
+        private static readonly CatanEvents _instance = new();
+        public static CatanEvents Instance
+        { get { return _instance; } }
+
+        
+
+        
+    }
+}
