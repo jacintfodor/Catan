059fac41983deb78c55d351d42f7e03ae67e70c5
y86qk2 <y86qk2@INF.ELTE.HU>
2022-05-03 17:00:17 +0200

    fix EndTurn to be compatible with refactor

--- Catan.Model/GameStates/ConcreteStates/MainState.cs
+++ Catan.Model/GameStates/ConcreteStates/MainState.cs
@@ -19,7 +19,7 @@ namespace Catan.Model.GameStates.ConcreteStates
             //TODO check winner
 
             context.NextPlayer();
-            context.Events.OnPlayer(context);
+            context.OnPlayer(context);
 
             //context.Events.OnBuildableByPlayer(context);
             context.SetContext(new RollingState());
