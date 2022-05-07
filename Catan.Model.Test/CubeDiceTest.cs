b151b24cadb0964c65040a38f904d6842850ea9c
Attila <azrc41@inf.elte.hu>
2022-05-07 20:48:33 +0200

    CubeDice test refactor

--- Catan.Model.Test/CubeDiceTest.cs
+++ Catan.Model.Test/CubeDiceTest.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Catan.Model.Context;
-using System.IO;
-using Catan.Model.Enums;
+
 namespace Catan.Model.Test
 {
     [TestClass]
@@ -15,24 +9,26 @@ namespace Catan.Model.Test
         [TestMethod]
         public void RollTest()
         {
+            //Arrange
             ICubeDice dice = new CubeDice(155);
             Assert.AreEqual(1, dice.RolledValue);
-            int sum = 0;
+            float sum = 0;
             int x = 100000;
+            float tolerance = 0.02f;
+            float expectedValue = 3.5f;
+
+            //Act
             for (int i = 0; i < x; i++)
             {
                 dice.roll();
                 sum += dice.RolledValue;
                 Assert.IsTrue(1 <= dice.RolledValue && dice.RolledValue <= 6);
             }
-            float sum2 = sum;
-            float f = sum2 / x;
-            float tolerance = 0.02f;
-            // 0.01 el is mindíg lefutott de a békesség kedvéért legyen 0.02
-            float expectedValue = 3.5f;
+            float f = sum / x;
+
+            //Assert
             Assert.IsTrue(expectedValue - tolerance < f);
             Assert.IsTrue(f < expectedValue + tolerance);
-
         }
     }
 }
