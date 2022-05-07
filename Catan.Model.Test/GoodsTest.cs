90d597e57713ded1dca0c2af5eb2491ac21a2900
Attila <azrc41@inf.elte.hu>
2022-05-07 20:49:31 +0200

    Goods test refactor

--- Catan.Model.Test/GoodsTest.cs
+++ Catan.Model.Test/GoodsTest.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using Catan.Model.Context;
 using System.Collections.Generic;
 using System.IO;
@@ -13,146 +12,140 @@ namespace Catan.Model.Test
         [TestMethod]
         public void ResourceEnumConstructor()
         {
+            //Arrange
             ResourceEnum e = ResourceEnum.Crop;
-            Goods goods1 = new Goods(e);
+            Goods goods1 = new(e);
 
+            //Assert
             Assert.IsNotNull(goods1);
-
             Assert.AreEqual(1, goods1.Crop);
             Assert.AreEqual(0, goods1.Ore);
             Assert.AreEqual(0, goods1.Wood);
             Assert.AreEqual(0, goods1.Brick);
             Assert.AreEqual(0, goods1.Wool);
-
             Assert.IsTrue(goods1.Valid);
         }
 
         [TestMethod]
         public void EmptyConstructor()
         {
-            Goods goods1 = new Goods();
+            Goods goods1 = new();
 
             Assert.IsNotNull(goods1);
-
             Assert.AreEqual(0, goods1.Crop);
             Assert.AreEqual(0, goods1.Ore);
             Assert.AreEqual(0, goods1.Wood);
             Assert.AreEqual(0, goods1.Brick);
             Assert.AreEqual(0, goods1.Wool);
-
             Assert.IsTrue(goods1.Valid);
         }
 
         [TestMethod]
         public void ConstructorWithList()
         {
-            List<int> list1 = new List<int>() { 0, 1, 2, 3, 4 };
-
-            Goods goods1 = new Goods(list1);
+            List<int> list1 = new() { 0, 1, 2, 3, 4 };
+            Goods goods1 = new(list1);
 
             Assert.IsNotNull(goods1);
-
             Assert.AreEqual(0, goods1.Crop);
             Assert.AreEqual(1, goods1.Ore);
             Assert.AreEqual(2, goods1.Wood);
             Assert.AreEqual(3, goods1.Brick);
             Assert.AreEqual(4, goods1.Wool);
-
             Assert.IsTrue(goods1.Valid);
         }
 
         [TestMethod]
         public void ConstructorWithWrongList()
         {
-            List<int> list1 = new List<int>() { -1, 1, 2, 3, 4 };
-
-            Goods goods1 = new Goods(list1);
+            List<int> list1 = new() { -1, 1, 2, 3, 4 };
+            Goods goods1 = new(list1);
 
             Assert.IsNotNull(goods1);
-
             Assert.AreEqual(-1, goods1.Crop);
             Assert.AreEqual(1, goods1.Ore);
             Assert.AreEqual(2, goods1.Wood);
             Assert.AreEqual(3, goods1.Brick);
             Assert.AreEqual(4, goods1.Wool);
-
             Assert.IsFalse(goods1.Valid);
         }
 
         [TestMethod]
         public void OperationPlus()
         {
-            List<int> list1 = new List<int>() { 2, 2, 2, 2, 2 };
-            List<int> list2 = new List<int>() { 1, 2, 3, 4, 5 };
+            //Arrange
+            List<int> list1 = new() { 2, 2, 2, 2, 2 };
+            List<int> list2 = new() { 1, 2, 3, 4, 5 };
+            Goods goods1 = new(list1);
+            Goods goods2 = new(list2);
 
-            Goods goods1 = new Goods(list1);
-            Goods goods2 = new Goods(list2);
-            goods1 = goods1 + goods2;
+            //Act
+            goods1 += goods2;
 
+            //Assert
             Assert.AreEqual(3, goods1.Crop);
             Assert.AreEqual(4, goods1.Ore);
             Assert.AreEqual(5, goods1.Wood);
             Assert.AreEqual(6, goods1.Brick);
             Assert.AreEqual(7, goods1.Wool);
-
             Assert.IsTrue(goods1.Valid);
         }
 
         [TestMethod]
         public void OperationMinus()
         {
-            List<int> list1 = new List<int>() { 2, 2, 2, 2, 2 };
-            List<int> list2 = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> list1 = new() { 2, 2, 2, 2, 2 };
+            List<int> list2 = new() { 1, 2, 3, 4, 5 };
+            Goods goods1 = new(list1);
+            Goods goods2 = new(list2);
 
-            Goods goods1 = new Goods(list1);
-            Goods goods2 = new Goods(list2);
-            goods1 = goods1 - goods2;
+            goods1 -= goods2;
 
             Assert.AreEqual(1, goods1.Crop);
             Assert.AreEqual(0, goods1.Ore);
             Assert.AreEqual(-1, goods1.Wood);
             Assert.AreEqual(-2, goods1.Brick);
             Assert.AreEqual(-3, goods1.Wool);
-
             Assert.IsFalse(goods1.Valid);
-
-            goods1 = goods1 + goods2;
-
-            Assert.AreEqual(2, goods1.Crop);
-            Assert.AreEqual(2, goods1.Ore);
-            Assert.AreEqual(2, goods1.Wood);
-            Assert.AreEqual(2, goods1.Brick);
-            Assert.AreEqual(2, goods1.Wool);
-
-            Assert.IsTrue(goods1.Valid);
         }
-
         [TestMethod]
-        public void OperationMultiplication()
+        public void OperationMultiplicationValid()
         {
-            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5 };
-            Goods goods1 = new Goods(list1);
-            goods1 = goods1 * 2;
+            List<int> list1 = new() { 1, 2, 3, 4, 5 };
+            Goods goods1 = new(list1);
+            goods1 *= 2;
 
             Assert.AreEqual(2, goods1.Crop);
             Assert.AreEqual(4, goods1.Ore);
             Assert.AreEqual(6, goods1.Wood);
             Assert.AreEqual(8, goods1.Brick);
             Assert.AreEqual(10, goods1.Wool);
-
             Assert.IsTrue(goods1.Valid);
+        }
 
-            goods1 = goods1 * -1;
+        [TestMethod]
+        public void OperationMultiplicationInvalid()
+        {
+            List<int> list1 = new() { 1, 2, 3, 4, 5 };
+            Goods goods1 = new(list1);
 
-            Assert.AreEqual(-2, goods1.Crop);
-            Assert.AreEqual(-4, goods1.Ore);
-            Assert.AreEqual(-6, goods1.Wood);
-            Assert.AreEqual(-8, goods1.Brick);
-            Assert.AreEqual(-10, goods1.Wool);
+            goods1 *= -1;
 
+            Assert.AreEqual(-1, goods1.Crop);
+            Assert.AreEqual(-2, goods1.Ore);
+            Assert.AreEqual(-3, goods1.Wood);
+            Assert.AreEqual(-4, goods1.Brick);
+            Assert.AreEqual(-5, goods1.Wool);
             Assert.IsFalse(goods1.Valid);
+        }
 
-            goods1 = goods1 * 0;
+        [TestMethod]
+        public void OperationMultiplicationZero()
+        {
+            List<int> list1 = new() { 1, 2, 3, 4, 5 };
+            Goods goods1 = new(list1);
+
+            goods1 *= 0;
 
             Assert.AreEqual(0, goods1.Crop);
             Assert.AreEqual(0, goods1.Ore);
@@ -167,8 +160,8 @@ namespace Catan.Model.Test
         [ExpectedException(typeof(InvalidDataException), "mismatching list count")]
         public void ConstructorListWithWrongLength()
         {
-            List<int> wrongList = new List<int>() { 1, 2, 3, 4, 5, 6 };
-            Goods goods = new Goods(wrongList);
+            List<int> wrongList = new() { 1, 2, 3, 4, 5, 6 };
+            Goods goods = new(wrongList);
         }
     }
 }
