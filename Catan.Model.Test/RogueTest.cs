ffd4a95ecdbba3868a58c6fe40d1aa6b4f87a76e
Attila <azrc41@inf.elte.hu>
2022-05-07 20:50:53 +0200

    Rogue test refactor

--- Catan.Model.Test/RogueTest.cs
+++ Catan.Model.Test/RogueTest.cs
@@ -1,32 +1,29 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Catan.Model.Context;
 
-
 namespace Catan.Model.Test
 {
     [TestClass]
     public class RogueTest
     {
         [TestMethod]
-        public void MoveTest()
+        public void Default()
         {
-            Rogue.Instance.Move(0, 0);
-
             Assert.AreEqual(0, Rogue.Instance.Row);
             Assert.AreEqual(0, Rogue.Instance.Col);
-
-            Rogue.Instance.Move(4, 1);
-
-            Assert.AreEqual(4, Rogue.Instance.Row);
-            Assert.AreEqual(1, Rogue.Instance.Col);
-
-            Rogue.Instance.Move(3, 5);
-
-            Assert.AreEqual(3, Rogue.Instance.Row);
-            Assert.AreEqual(5, Rogue.Instance.Col);
-
         }
 
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 1)]
+        [DataRow(3, 3)]
+        [DataRow(4, 1)]
+        [DataRow(3, 5)]
+        public void MoveTest(int x, int y)
+        {
+            Rogue.Instance.Move(x, y);
+            Assert.AreEqual(x, Rogue.Instance.Row);
+            Assert.AreEqual(y, Rogue.Instance.Col);
+        }
     }
-
 }
