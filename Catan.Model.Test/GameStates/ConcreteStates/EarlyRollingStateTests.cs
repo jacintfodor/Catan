b89f4f4e6ec7d9c679483088171f11ce6d61632f
jacintfodor <66223743+jacintfodor@users.noreply.github.com>
2022-05-18 11:42:04 +0200

    Create EarlyRollingState test

---
+++ Catan.Model.Test/GameStates/ConcreteStates/EarlyRollingStateTests.cs
@@ -0,0 +1,42 @@
+﻿using Catan.Model.GameStates.ConcreteStates;
+using Moq;
+using System;
+using Xunit;
+
+namespace Catan.Model.Test.GameStates.ConcreteStates
+{
+    public class EarlyRollingStateTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        public EarlyRollingStateTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        private EarlyRollingState CreateEarlyRollingState()
+        {
+            return new EarlyRollingState();
+        }
+
+        [Fact]
+        public void RollDices_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var earlyRollingState = this.CreateEarlyRollingState();
+            ICatanContext context = null;
+
+            // Act
+            earlyRollingState.RollDices(
+                context);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
